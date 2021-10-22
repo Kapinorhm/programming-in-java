130c1f310b26e8f889ac9ff4a3575396a19e1915
Kapinorhm <samy.ait.ssi@efrei.net>
2021-10-22 08:06:08 +0200

    Interfaces commit

--- proj1/src/main/java/agh/ii/prinjava/proj1/MyQueue.java
+++ proj1/src/main/java/agh/ii/prinjava/proj1/MyQueue.java
@@ -2,20 +2,50 @@ package agh.ii.prinjava.proj1;
 
 import agh.ii.prinjava.proj1.impl.MyQueueDLLBImpl;
 
+/**
+ * MyQueue is an interface for creating and managing a queue data structure based on a double linked list.
+ * @param <E>, Whatever the type of the element stored in the queue is, generic represents it.
+ */
 public interface MyQueue<E> {
+    /**
+     * A method for adding a new element to our queue.
+     * @param x, the element that will be added to the queue
+     */
     void enqueue(E x);
 
+    /**
+     * To remove the initial element, we use the queue method.
+     * @return el, the element that was taken out of the queue
+     */
     E dequeue();
 
+    /**
+     * the element that was taken out of the queue
+     * @return a boolean value that is false if our queue is still full and true if it is empty.
+     */
     default boolean isEmpty() {
         return numOfElems() == 0;
     }
 
+    /**
+     * A function to determine the number of elements in our queue.
+     * @return el_number, the queue's total number of elements
+     */
+
     int numOfElems();
 
+    /**
+     * A function that returns the value of the first item in the queue (the next element that will be removed)
+     * @return myPeek, the highest value in the queue
+     */
     E peek();
 
-    /** Consider pros and cons of having a factory method in the interface */
+    /** Consider the advantages and disadvantages of including a factory method in the interface. */
+    /**
+     * A function for creating a data structure for a queue.
+     * @param <T> the elements held in myQueue's type
+     * @return MyQueueDLLBImpl, a new queue has been created
+     */
     static <T> MyQueue<T> create() {
         return new MyQueueDLLBImpl<>();
     }
--- proj1/src/main/java/agh/ii/prinjava/proj1/MyStack.java
+++ proj1/src/main/java/agh/ii/prinjava/proj1/MyStack.java
@@ -2,20 +2,51 @@ package agh.ii.prinjava.proj1;
 
 import agh.ii.prinjava.proj1.impl.MyStackDLLBImpl;
 
+/**
+ * MyStack is a user interface for creating and managing a stack data structure based on a double linked list.
+ * @param <E>, Whatever the type of the element stored in the stack is, generic represents it.
+ */
+
 public interface MyStack<E> {
+    /**
+     * The pop method will remove the element from the top of the stack.
+     * @return E, the value of the element that was removed
+     */
     E pop();
 
+    /**
+     * The method push adds a new element to the stack's top.
+     * @param x, the component to include in our stack
+     */
     void push(E x);
 
+    /**
+     *The function that determines whether or not there are any more elements in our Stack.
+     * @return a boolean value: false if our stack has any more elements, true if it is empty
+     */
     default boolean isEmpty() {
         return numOfElems() == 0;
     }
 
+    /**
+     * A function that tells us how many elements are in the stack right now.
+     * @return The number of elements in the stack, el number
+     */
+
     int numOfElems();
 
+    /**
+     * A function for determining the value at the very top of a stack.
+     * @return myPeek, the value at the top of the stack
+     */
     E peek();
 
-    /** Consider pros and cons of having a factory method in the interface */
+    /** Consider the advantages and disadvantages of including a factory method in the interface. */
+    /**
+     * Creates a stack data structure with a function.
+     * @param <T>, the type of the piece in myStack that is stored
+     * @return The new stack built is called MyStackDLLBImpl.
+     */
     static <T> MyStack<T> create() {
         return new MyStackDLLBImpl<T>();
     }
